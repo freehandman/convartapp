da8b8cadddc26f596b19205f86cd636a7b8b953e
freehandman <cypriannziim@gmail.com>
2018-06-30 09:48:05 +0100

    Updates

--- sw.js
+++ sw.js
@@ -62,20 +62,20 @@ self.addEventListener('install', event => {
 });
 
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('static-') &&
-                 !allCaches.includes(cacheName);
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
+// self.addEventListener('activate', function(event) {
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.filter(function(cacheName) {
+//           return cacheName.startsWith('static-') &&
+//                  !allCaches.includes(cacheName);
+//         }).map(function(cacheName) {
+//           return caches.delete(cacheName);
+//         })
+//       );
+//     })
+//   );
+// });
 
 // self.addEventListener('fetch', function (event) {
 //   let requestUrl = new URL(event.request.url);
